4d19a539da8bd299370f04ec3264c9c45e7a281a
Tai Cai <taicai@rtanalytics.vn>
2025-11-30 21:03:18 +0700

    feat: add create_realtimex_deep_agent function and update __all__ export

--- libs/deepagents/deepagents/__init__.py
+++ libs/deepagents/deepagents/__init__.py
@@ -3,5 +3,6 @@
 from deepagents.graph import create_deep_agent
 from deepagents.middleware.filesystem import FilesystemMiddleware
 from deepagents.middleware.subagents import CompiledSubAgent, SubAgent, SubAgentMiddleware
+from deepagents.realtimex_graph import create_realtimex_deep_agent
 
-__all__ = ["CompiledSubAgent", "FilesystemMiddleware", "SubAgent", "SubAgentMiddleware", "create_deep_agent"]
+__all__ = ["CompiledSubAgent", "FilesystemMiddleware", "SubAgent", "SubAgentMiddleware", "create_deep_agent", "create_realtimex_deep_agent"]
