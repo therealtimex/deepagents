b45fbfd4dacfae1d0772c2b21e6b87cccd47e698
Eugene Yurtsev <eugene@langchain.dev>
2026-02-02 10:27:33 -0500

    feat(sdk): add LocalShellBackend (#930)
    This PR adds LocalShellBackend, which extends FilesystemBackend to
    support shell command execution on the local host system. The backend
    implements the SandboxBackendProtocol.

--- libs/deepagents/deepagents/backends/__init__.py
+++ libs/deepagents/deepagents/backends/__init__.py
@@ -2,6 +2,7 @@
 
 from deepagents.backends.composite import CompositeBackend
 from deepagents.backends.filesystem import FilesystemBackend
+from deepagents.backends.local_shell import LocalShellBackend
 from deepagents.backends.protocol import BackendProtocol
 from deepagents.backends.state import StateBackend
 from deepagents.backends.store import StoreBackend
@@ -10,6 +11,7 @@ __all__ = [
     "BackendProtocol",
     "CompositeBackend",
     "FilesystemBackend",
+    "LocalShellBackend",
     "StateBackend",
     "StoreBackend",
 ]
